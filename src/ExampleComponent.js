4bc099a963931b6e528cb0e8b489ce432fb02d23
DDuongNguyen <duong.nguyen4991@gmail.com>
2019-07-15 14:12:36 -0400

    Done.

--- src/ExampleComponent.js
+++ src/ExampleComponent.js
@@ -5,8 +5,9 @@ class ExampleComponent extends Component {
   render() {
     return (
       <div className="example">
-        <img src="https://s3.amazonaws.com/ironboard-learn/sunglasses.gif" alt="whoa"/>
-        <p>Whoa!</p>
+        <img src="https://lh3.googleusercontent.com/y0UpK3E3SgYq7LYQFFM5vYBl9amBVIOQ4NHlbBBeB4MZh2bj3fXpOA6eRjP2r-x3OgvLHLBwLnF8AzvoEJHyccY_T5G4QNpduq-54VCB5DkWhLeHiFXiZLXbRvXhQ_Clviz9xG5nZjc5PyHk2GHjEzS3wIK5MT9R2Fik5ndNOLj9GAQx7Mx0NFdNngwpoZiY9PtgZukTBxIRTEQP-QGbKn7JMVf-JqIHsuIOgxogofJC-n7kHnOPcajmnKtZi5ckbBeJx1m2cCEqS7zHEwqDHcRMhGWbpmnIs-zoyvMKWYM2md3TpfrDgMXWxwgEqxS-eXk21QodygDg231HV2b8ZLn0SzzmiNlSsyINPzgewBAnzOGwSfHt6xskM91DgflYmLXnuyS1jjIf2EFdTGuWazfJ5TRTMByYUvbl-zuAoGFuEQRYK1XQDqmirR02LJGINoM_aT6MbiwpI9diZStel-ZmtWBE22VR1xjrIaJPov23D9xK_J4oehXRVw3Y5OlE_ZyzsKLO6hbydhlEXv3e1OAkuwKEImMx86IqQ6jatGNL4AoIr1ZYV4VH1nsfqwgPYf2_hRUxsjnjHFxQ8sptFD1I5jsiJzcsPkk8s_XkpM8X3Ks7ruUgZiyxA8SxziMu8Q2uikVqvjLtplv_t3TJtxZ1s1Dx9pM=w360-h640-no" alt="whoa"/>
+        <h1>Nyan Desuka!</h1>
+        <h1>Subarashi</h1>
       </div>
     );
   }
